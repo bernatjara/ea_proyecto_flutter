78dc2c39546165dd6d282f880d1382696551198c
Bernat Jara <bernat.jara@estudiantat.upc.edu>
2023-11-16 11:15:18 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ea_proyecto_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ea_proyecto_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../widgets/register_form.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Registro')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RegisterForm(),
+          ],
+        ),
+      ),
+    );
+  }
+}
