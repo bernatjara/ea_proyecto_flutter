2cee0f9fbdd47d872e03a8fe8db4ac8e40596f76
Nil Velàsquez <nil.velasquez@estudiantat.upc.edu>
2023-11-18 12:48:16 +0100

    version 1

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -17,6 +17,17 @@ class UserScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(bottom: 700.0, right: 30.0),
+        child: FloatingActionButton(
+          backgroundColor: Color.fromARGB(255, 47, 40, 109),
+          child: const Icon(Icons.add, color: Color(0xFFFFFCEA)),
+          onPressed: () {
+            //Navigator.pushNamed(context, '/create_user');
+          },
+          shape: CircleBorder(),
+        ),
+      ),
     );
   }
 }
